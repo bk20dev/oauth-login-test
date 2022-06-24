3517bacacd6bd5297155edda7ec44b903d942d87
Bartosz Bieniek <itsbk20@gmail.com>
2022-06-24 22:39:19 +0200

    Install chi

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module rabbit
 
 go 1.18
+
+require github.com/go-chi/chi v1.5.4
--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"github.com/go-chi/chi"
 	"log"
+	"net/http"
 )
 
 func main() {
-	log.Println("Hello world")
+	router := chi.NewRouter()
+
+	server := http.Server{
+		Addr:    ":4000",
+		Handler: router,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("failed to start a server: %v", err)
+	}
 }
